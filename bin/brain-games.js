c822028980e0259d6dba27ddfac5d197546e278f
khloptsev <p.khloptsev@gmail.com>
2020-03-02 12:56:34 +1000

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-import { greeting } from '../src/index.js';
+import greeting from '../src/index.js';
 
 // welcome
 console.log('Welcome to the Brain Games!');
 
 // greeting
 console.log(greeting());
-
