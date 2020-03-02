23b19cacd2c05819718aacf153de4716f7aeefdd
khloptsev <p.khloptsev@gmail.com>
2020-03-02 22:10:34 +1000

    fix lint error

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-import greeting from '../src/index.js';
 import game from '../src/brain-even-game.js';
 
 // welcome
 console.log('Welcome to the Brain Games!');
-
-// greeting
-// console.log(greeting());
 
 // game "Parity Check"
 console.log(game());
