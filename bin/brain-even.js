9dbc1e890e41441f40424d0fd930dcd7272ff216
khloptsev <p.khloptsev@gmail.com>
2020-03-02 22:02:34 +1000

    create and add code brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import greeting from '../src/index.js';
+import game from '../src/brain-even-game.js';
+
+// welcome
+console.log('Welcome to the Brain Games!');
+
+// greeting
+// console.log(greeting());
+
+// game "Parity Check"
+console.log(game());
