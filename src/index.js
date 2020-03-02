604ac11cadbff1a7c722cb0e49edbb7072991f7d
khloptsev <p.khloptsev@gmail.com>
2020-03-02 21:59:14 +1000

    add isEven func, update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const getName = () => {
   const name = readlineSync.question('May I have your name? ');
+  return name;
+};
+
+export const isEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+export default () => {
+  const name = getName();
   return `Hello, ${name}!`;
 };
