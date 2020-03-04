3fc1724d3edf8fe976b70dfdfa08e6980013ddc4
khloptsev <p.khloptsev@gmail.com>
2020-03-04 15:24:34 +1000

    add new game brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import welcome from '../src/index.js';
+import brainCalcGame from '../src/brain-calc-game.js';
+
+// welcome
+console.log(welcome());
+
+// game "Calculator"
+console.log(brainCalcGame());
