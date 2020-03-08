d33913b02ab4b1deb0781152237c66dce5689455
khloptsev <p.khloptsev@gmail.com>
2020-03-08 15:50:03 +1000

    update game engine

--- src/index.js
+++ src/index.js
@@ -1,60 +1,13 @@
-import readlineSync from 'readline-sync';
-import gameEven from '../games/brain-even-game.js';
-import gameCalc from '../games/brain-calc-game.js';
-import gameGcd from '../games/brain-gcd-game.js';
-import gameProgression from '../games/brain-progression-game.js';
-import gameIsPrime from '../games/brain-prime-game.js';
-
-// general
-const getName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
-};
-
-const isRight = (userAnswer, correctAnswer) => {
-  if (userAnswer !== correctAnswer) {
-    return false;
-  }
-  return true;
-};
-
-const correct = () => 'Correct!';
-
-const win = (name) => `Congratulations, ${name}!`;
-
-const lose = (userAnswer, correctAnswer, name) => `"${userAnswer}" is wrong asnwer ;(. Correct answer was "${correctAnswer}"\nLet's try again, ${name}!`;
-
-// game selection
-const getGame = (rule) => {
-  if (rule === 'Answer "yes" if the number is even, otherwise "no".') {
-    return gameEven();
-  }
-  if (rule === 'What is the result of the expression?') {
-    return gameCalc();
-  }
-  if (rule === 'Find the greatest common divisor of given numbers.') {
-    return gameGcd();
-  }
-  if (rule === 'What number is missing in the progression?') {
-    return gameProgression();
-  }
-  return gameIsPrime();
-};
-
-export const welcome = () => 'Welcome to the Brain Games!';
-
-// brain-games
-export const hello = () => {
-  const name = getName();
-  return `Hello, ${name}!`;
-};
+import {
+  correct, getName, isRight, lose, win, getGame, getRule,
+} from '../helper/utility.js';
 
 // game engine
-export default (ruleOfGame) => {
+export default (game) => {
   const name = getName();
-  console.log(ruleOfGame);
+  console.log(getRule(game));
   for (let i = 0; i < 3; i += 1) {
-    const question = getGame(ruleOfGame);
+    const question = getGame(game);
     const [userAnswer, correctAnswer] = question;
     if (isRight(userAnswer, correctAnswer)) {
       console.log(correct());
