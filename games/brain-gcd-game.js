ff0d937f5b9e26240e82a18974235cdc11b7c1a1
khloptsev <p.khloptsev@gmail.com>
2020-03-11 21:53:28 +1000

    move gcd func from math.js

--- games/brain-gcd-game.js
+++ games/brain-gcd-game.js
@@ -1,14 +1,25 @@
 import readlineSync from 'readline-sync';
 import pairs from '@hexlet/pairs';
-import { getNumber, gcd } from '../helper/math.js';
+import { getRandomNumber } from '../helper/math.js';
 
 // game name
 export const gameName = () => 'gcd';
 
+// greatest common devider two numbers
+const gcd = (num1, num2) => {
+  let result = 0;
+  for (let i = 1; i <= num1; i += 1) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      result = i;
+    }
+  }
+  return result;
+};
+
 // game
 export default () => {
   const result = [];
-  const pair = pairs.cons(getNumber(), getNumber());
+  const pair = pairs.cons(getRandomNumber(1, 101), getRandomNumber(1, 101));
   const correctAnser = gcd(pairs.car(pair), pairs.cdr(pair));
   const userAnswer = Number(readlineSync.question(`Question: ${pairs.car(pair)} ${pairs.cdr(pair)}\nYour answer: `));
   result.push(userAnswer, correctAnser);
