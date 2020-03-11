cae1aa2ff2d22fc9d22325228e4106fb401a8c9e
khloptsev <p.khloptsev@gmail.com>
2020-03-11 21:59:24 +1000

    move buildProgression func from math.js

--- games/brain-progression-game.js
+++ games/brain-progression-game.js
@@ -1,14 +1,26 @@
 import readlineSync from 'readline-sync';
-import { getSmallNumber, buildProgression } from '../helper/math.js';
+import getRandomNumber from '../helper/math.js';
 
 // game name
 export const gameName = () => 'progression';
+
+// buildMathProgression
+export const buildProgression = () => {
+  let begin = getRandomNumber(0, 101);
+  const step = Math.floor((Math.random() * 5) + 1);
+  const result = [];
+  for (let i = 0; i < 10; i += 1) {
+    result.push(begin);
+    begin += step;
+  }
+  return result;
+};
 
 // game
 export default () => {
   const result = [];
   const progression = buildProgression();
-  const index = getSmallNumber();
+  const index = getRandomNumber(0, 10);
   const correctAnswer = progression[index];
   const hideNumber = progression.map((item) => {
     let hidedItem = item;
