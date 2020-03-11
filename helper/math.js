6859a2956d0dc7d72caf2716315453b87209449a
khloptsev <p.khloptsev@gmail.com>
2020-03-11 22:00:24 +1000

    remove buildProgression func / export default randomNumber func

--- helper/math.js
+++ helper/math.js
@@ -1,17 +1,5 @@
 // get random number from range
-export const getRandomNumber = (min, max) => {
+export default (min, max) => {
   const randomize = Math.floor(Math.random() * (max - min) + min);
   return randomize;
-};
-
-// buildMathProgression
-export const buildProgression = () => {
-  let begin = getRandomNumber(0, 101);
-  const step = Math.floor((Math.random() * 5) + 1);
-  const result = [];
-  for (let i = 0; i < 10; i += 1) {
-    result.push(begin);
-    begin += step;
-  }
-  return result;
 };
