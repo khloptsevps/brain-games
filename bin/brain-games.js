024519f3a676b56843508882319e6cc0f9bfdd19
khloptsev <p.khloptsev@gmail.com>
2020-03-11 23:17:17 +1000

    fix func

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 import { welcome, hello } from '../helper/utility.js';
-import game from '../src/index.js';
 
 // welcome
 console.log(welcome());
 
 // greeting
-console.log(game(hello()));
+hello();
