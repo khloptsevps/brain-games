31734f53712cf3bba6f6c19c0efd8a04a3c2ba4a
khloptsev <p.khloptsev@gmail.com>
2020-03-13 10:53:06 +1000

    move functions from utility.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import {
-  correct, getName, isRight, lose, win, getGame, getRule, welcome,
-} from '../helper/utility.js';
+import readlineSync from 'readline-sync';
+import { getGame, getRule } from '../helper/utility.js';
+
+// Welcome message
+const welcome = () => '\nWelcome to the Brain Games!';
+
+// get user name
+const getName = () => {
+  const name = readlineSync.question('\nMay I have your name? ');
+  return name;
+};
+
+// if correctly answer
+const correct = () => 'Correct!';
+
+// user win
+const win = (name) => `Congratulations, ${name}!`;
+
+// user lose
+const lose = (userAnswer, correctAnswer, name) => `\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${correctAnswer}"\nLet's try again, ${name}!`;
+
+// answers check
+const isRight = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) {
+    return true;
+  }
+  return false;
+};
 
 // game engine
 export default (game) => {
