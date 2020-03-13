8901ba8afdf607d64cff07562abd734c9c7dbe1c
khloptsev <p.khloptsev@gmail.com>
2020-03-13 19:17:13 +1000

    update game launch

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import game from '../src/index.js';
-import { gameName } from '../games/brain-even-game.js';
+import startGame from '../games/brain-even-game.js';
 
 // game "Parity-Check"
-game(gameName());
+startGame();
