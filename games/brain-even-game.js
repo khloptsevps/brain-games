2095f31a5d4cef5779b9da5604a413c390e0b85e
khloptsev <p.khloptsev@gmail.com>
2020-03-13 19:16:35 +1000

    update game logic

--- games/brain-even-game.js
+++ games/brain-even-game.js
@@ -1,8 +1,5 @@
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../helper/math.js';
-
-// game name
-export const gameName = () => 'even';
+import gameEngine from '../src/index.js';
 
 // even number check
 const isEven = (num) => {
@@ -12,17 +9,21 @@ const isEven = (num) => {
   return false;
 };
 
+// game rule
+const gameRule = () => '\nAnswer "yes" if the number is even, otherwise "no".\n';
+
 // game
-export default () => {
+const gameEven = () => {
   const result = [];
   const number = getRandomNumber(1, 101);
-  const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `);
   let correctAnswer;
   if (isEven(number)) {
     correctAnswer = 'yes';
   } else {
     correctAnswer = 'no';
   }
-  result.push(userAnswer, correctAnswer);
+  result.push(number, correctAnswer);
   return result;
 };
+
+export default () => gameEngine(gameRule(), gameEven);
