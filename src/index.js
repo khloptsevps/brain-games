7714a3f1f42fbc485a388eeaf5145f0bed0825b3
khloptsev <p.khloptsev@gmail.com>
2020-03-17 13:01:30 +1000

    update game engine. remove extra func

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import readlineSync from 'readline-sync';
 
-// Welcome message
-const welcome = () => '\nWelcome to the Brain Games!';
-
-// get user name
-const getName = () => {
-  const name = readlineSync.question('\nMay I have your name? ');
-  return name;
-};
-
-// answers check
-const isRight = (userAnswer, correctAnswer) => {
-  if (userAnswer === correctAnswer) {
-    return true;
-  }
-  return false;
-};
-
 // game engine
 export default (gameRule, getQuestion) => {
-  console.log(welcome());
-  const name = getName();
+  console.log('\nWelcome to the Brain Games!');
+  const name = readlineSync.question('\nMay I have your name? ');
   console.log(gameRule);
   for (let i = 0; i < 3; i += 1) {
     const question = getQuestion();
     const [expression, correctAnswer] = question;
     const userAnswer = readlineSync.question(`Question: ${expression}\nYour answer: `);
-    if (isRight(userAnswer, correctAnswer)) {
+    if (userAnswer === correctAnswer) {
       console.log('Correct!\n');
     } else {
       return console.log(`\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${correctAnswer}"\nLet's try again, ${name}!`);
