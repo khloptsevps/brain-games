6cb42991db253459dc93a77dd3d4eaf83061d52d
khloptsev <p.khloptsev@gmail.com>
2020-03-17 16:33:04 +1000

    rename start func

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import startGame from '../src/games/brain-even-game.js';
+import gameStart from '../src/games/brain-even-game.js';
 
 // game "Parity-Check"
-startGame();
+gameStart();
