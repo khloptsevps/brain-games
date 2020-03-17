8a52cbba6ace0f7c90c15d03e791220766499ce8
khloptsev <p.khloptsev@gmail.com>
2020-03-17 12:51:17 +1000

    fix func

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,5 +1,5 @@
 // get random number from range
 export default (min, max) => {
-  const randomize = Math.floor(Math.random() * (max - min) + min);
+  const randomize = Math.round(Math.random() * (max - min) + min);
   return randomize;
 };
