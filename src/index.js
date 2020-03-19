5336eaa6c5dda70547c1b1004673376b0147fa6b
khloptsev <p.khloptsev@gmail.com>
2020-03-19 15:09:36 +1000

    fix question func in gameEngine. rename in generateQuestionAndAnswer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import readlineSync from 'readline-sync';
 
 // game engine
-export default (gameRule, getQuestion) => {
+export default (task, generateQuestionAndAnswer) => {
   console.log('\nWelcome to the Brain Games!');
   const name = readlineSync.question('\nMay I have your name? ');
-  console.log(gameRule);
-  for (let i = 0; i < 3; i += 1) {
-    const question = getQuestion();
-    const [expression, correctAnswer] = question;
-    const userAnswer = readlineSync.question(`Question: ${expression}\nYour answer: `);
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!\n');
-    } else {
-      return console.log(`\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${correctAnswer}"\nLet's try again, ${name}!`);
+  console.log(task);
+  const roundsCount = 3;
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [question, answer] = generateQuestionAndAnswer();
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    if (userAnswer !== answer) {
+      return `\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${answer}"\nLet's try again, ${name}!`;
     }
+    console.log('Correct!\n');
   }
-  return console.log(`Congratulations, ${name}!`);
+  return `Congratulations, ${name}!`;
 };
