1242648b044a4e69c9919b91551fb0425af771de
khloptsev <p.khloptsev@gmail.com>
2020-03-24 10:40:23 +1000

    update game engine, update logic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
+
 // game engine
 export default (task, generateQuestionAndAnswer) => {
-  console.log('\nWelcome to the Brain Games!');
-  const name = readlineSync.question('\nMay I have your name? ');
-  console.log(task);
-  const roundsCount = 3;
+  console.log('\nWelcome to the Brain Games!\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+  console.log(`${task}\n`);
   for (let i = 1; i <= roundsCount; i += 1) {
     const [question, answer] = generateQuestionAndAnswer();
-    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== answer) {
-      return `\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${answer}"\nLet's try again, ${name}!`;
+      console.log(`\n"${userAnswer}" is wrong asnwer ;(. Correct answer was "${answer}"`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
     console.log('Correct!\n');
   }
-  return `Congratulations, ${name}!`;
+  console.log(`Congratulations, ${name}!`);
 };
